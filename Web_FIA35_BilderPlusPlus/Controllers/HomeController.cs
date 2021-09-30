85d16461414bc85dc41b5f00ea7c5fcb2cad48e0
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-09-30 11:20:36 +0200

    Projektdateien hinzufügen.

---
+++ Web_FIA35_BilderPlusPlus/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_FIA35_BilderPlusPlus.Models;
+
+namespace Web_FIA35_BilderPlusPlus.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Hierin befinden sich alle Informationen zur Umgebung des Webprojekts
+        // "readonly" bewirkt, dass es keine Änderungen geben kann (außer durch den Konstruktor beim Erzeugen der neuen Instanz am Anfang)
+        // "private" müsste nicht extra gesetzt werden, weil alle Methoden per Standard auf "private" gesetzt sind
+        private readonly IWebHostEnvironment Environment;
+
+        public HomeController(IWebHostEnvironment _environment)  // Konstruktor ist "public", weil sonst keine Instanz von außen erzeugt wird
+        {
+            Environment = _environment;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+            [HttpPost]
+        public IActionResult Index(FilterSortBildList filterSortBildList)
+        {
+            // Umgebungsinformationen zur Pfadermittlung der Bilder nutzen
+            string wwwPath = this.Environment.WebRootPath;
+            string contentPath = this.Environment.ContentRootPath;
+
+            DirectoryInfo di = new DirectoryInfo(wwwPath + "\\Bilder");
+            FileInfo[] fi = di.GetFiles("*.*");
+            List<FileInfo> DateiListe = new List<FileInfo>(fi);
+
+            // Model mit Liste der Dateiinfos
+            FilterSortBildList Infos = new FilterSortBildList { listDateiInfos = DateiListe };
+
+            return View(Infos);
+        }
+    }
+}
---
+++ Web_FIA35_BilderPlusPlus/Models/FilterSortBildList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_FIA35_BilderPlusPlus.Models
+{
+    public class FilterSortBildList
+    {
+        public string sortierTyp { get; set; }
+        public string sortierRichtung { get; set; }
+        public List<string> filterBildTyp { get; set; }
+        public List<FileInfo> listDateiInfos { get; set; }
+    }
+}
